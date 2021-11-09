f1305ef622d771b597a942dd4cc597c0ad5613c6
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-11-10 00:22:59 +0600

    Bismillah

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nebgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+async function run() {
+   try {
+      await client.connect();
+      console.log("connected to db");
+   } finally {
+      // await client.close();
+   }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+   res.send("assignment 12 server is running");
+});
+
+app.listen(port, () => {
+   console.log("assignment 12 server port running on", port);
+});
